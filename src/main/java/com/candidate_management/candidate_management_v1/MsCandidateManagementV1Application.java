9830b618dd6ad14d15cfe86215b39deefc480605
jose mendoza sanchez <jmendozarv@outlook.com>
2024-06-28 22:31:45 -0500

    initial commit  - files

---
+++ src/main/java/com/candidate_management/candidate_management_v1/MsCandidateManagementV1Application.java
@@ -0,0 +1,13 @@
+package com.candidate_management.candidate_management_v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCandidateManagementV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCandidateManagementV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/candidate_management/candidate_management_v1/MsCandidateManagementV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.candidate_management.candidate_management_v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCandidateManagementV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
